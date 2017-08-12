0589012c4371f82da9b93fccfad3ba2228a6b92d
DM45 <mda.tomsk@gmail.com>
2017-08-12 13:16:38 +0700

    Решение задачи

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,23 @@
+import os
+import re
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as file_handler:
+        return file_handler.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    words = re.findall(r'\w+', text.lower())
+    most_freq_words = [word[0] for word in Counter(words).most_common(10)]
+    return ' '.join(most_freq_words)
 
 
 if __name__ == '__main__':
-    pass
+    _filepath = input('Enter filepath to file: ')
+    _load_data = load_data(_filepath)
+    print('List of 10 frequent words: ')
+    print(get_most_frequent_words(_load_data))
