90dced1e3cf0e30c0cbbe3b41cbb6adbe19bfeb1
DM45 <mda.tomsk@gmail.com>
2017-08-12 13:38:05 +0700

    Исправление ошибок

--- lang_frequency.py
+++ lang_frequency.py
@@ -10,14 +10,17 @@ def load_data(filepath):
         return file_handler.read()
 
 
-def get_most_frequent_words(text):
+def get_most_frequent_words(text, top_count):
     words = re.findall(r'\w+', text.lower())
-    most_freq_words = [word[0] for word in Counter(words).most_common(10)]
+    most_freq_words = [
+        word[0] for word in
+        Counter(words).most_common(top_count)]
     return ' '.join(most_freq_words)
 
 
 if __name__ == '__main__':
     _filepath = input('Enter filepath to file: ')
+    _top_count = int(input('Enter count of top words: '))
     _load_data = load_data(_filepath)
-    print('List of 10 frequent words: ')
-    print(get_most_frequent_words(_load_data))
+    print('List of ' + str(_top_count) + ' frequent words: ')
+    print(get_most_frequent_words(_load_data, _top_count))
